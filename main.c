d6cb0a61cd1454965fa49788632a862470cc4f0f
z2yeon <55427811+z2yeon@users.noreply.github.com>
2019-12-06 15:15:02 +0900

    12/06 15:15

---
+++ main.c
@@ -0,0 +1,202 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "storage.h"
+
+#define N_BUILDING				10
+#define N_FLOOR					10
+#define N_ROOM					10
+
+#define STORAGE_FILEPATH 		"storage.txt"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+//get an integer value from keyboard
+int getIntegerInput(void)
+{
+	int input = -1;
+	scanf("%d", &input);
+	fflush(stdin);
+	
+	return input;
+}
+
+
+
+//check whether building/room number is valid or not
+int buildingValidityCheck(int nBuilding, int nRoom)
+{
+	if (nBuilding <= 0 || nBuilding > N_BUILDING)
+	{
+		return -1;
+	}
+	
+	if (nRoom/100 <= 0 || nRoom/100 > N_FLOOR)
+	{
+		return -1;
+	}
+	
+	if (nRoom%100 <= 0 || nRoom%100 > N_ROOM)
+	{
+		return -1;
+	}
+	
+	return 0;
+}
+
+
+int main(int argc, char *argv[]) {
+	
+	
+	int cmd;	//command
+	int x, y, nBuilding, nRoom;
+	char msg[MAX_MSG_SIZE+1];	//message
+	char passwd[PASSWD_LEN+1];	//password
+	
+	//1. initialize the delivery system
+	if (str_createSystem(STORAGE_FILEPATH) != 0)
+	{
+		printf("failed to open storage config file! (%s)\n", STORAGE_FILEPATH);
+		return -1;
+	}
+	
+	printf("------- Unmanned Delivery Storage System START -------\n");
+	
+	do
+	{
+        //2. menu printing
+		printf("\n\n");
+		printf("----------------------------------------------\n");
+		printf("1. put a package\n");
+		printf("2. get my package\n");
+		printf("3. check the storage status\n");
+		printf("4. find my package\n");
+		printf("0. exit\n");
+		printf("----------------------------------------------\n");
+		printf("command : ");
+		
+        //3. getting user command
+		cmd = getIntegerInput();	
+		
+		switch(cmd)
+		{
+            //4-1. command analysis : exit the program
+			case 0:
+				printf(" -----------> exiting the program....\n");
+				break;
+			
+            //4-2. command analysis : put package
+			case 1:
+                //provide the current status
+				str_printStorageStatus();
+				
+                //storage cell selection
+				printf(" - storage row : ");
+				x = getIntegerInput();
+				printf(" - storage column : ");
+				y = getIntegerInput();
+				if (str_checkStorage(x,y) != 0)
+				{
+					printf(" -----------> Storage (%i,%i) is already occupied or invalid!\n", x, y);
+					continue;
+				}
+				
+                //destination input
+				printf(" - building # : ");
+				nBuilding = getIntegerInput();
+				printf(" - room # : ");
+				nRoom = getIntegerInput();
+				if (buildingValidityCheck(nBuilding, nRoom) != 0)
+				{
+					printf(" -----------> Invalid building/room number (%i,%i)\n", nBuilding, nRoom);
+					continue;
+				}
+				
+                //package context input (message string)
+				printf(" - message (max : 100 characters) : ");
+				scanf("%100s", msg);
+				fflush(stdin);
+				
+                //password input
+				passwd[0] = 0;
+				printf(" - password : ");
+				scanf("%4s", passwd);
+				fflush(stdin);
+				
+                //put the package in the cell
+				printf(" -----------> processing to put the package...\n\n");
+				if (str_pushToStorage(x, y, nBuilding, nRoom, msg, passwd) == 0)
+				{
+					printf(" -----------> Succeeded to put package for (%i, %i) user in (%i,%i), msg : %s passwd : %s\n", nBuilding, nRoom, x, y, msg, passwd);
+				}
+				else
+				{
+					printf(" -----------> Failed to put the package in the storage!\n");
+				}
+				break;
+                
+			//4-3. command analysis : extract my package
+			case 2:
+                //input the storage cell
+				printf(" - storage row : ");
+				x = getIntegerInput();
+				printf(" - storage column : ");
+				y = getIntegerInput();
+				
+                //check the input storage cell
+				if (str_checkStorage(x,y) <= 0)
+				{
+					printf(" -----------> Storage (%i,%i) is empty or invalid!\n", x, y);
+					continue;
+				}
+                //try to extract the cell
+				if (str_extractStorage(x,y) != 0)
+				{
+					printf(" -----------> Failed to extract my package!\n");
+				}
+				break;
+				
+            //4-4. command analysis : print the storage status
+			case 3:
+				str_printStorageStatus();
+				break;
+                
+			//4-4. command analysis : find my package from the storage
+			case 4:
+                //input the destination (my address)
+				printf(" - building # : ");
+				nBuilding = getIntegerInput();
+				printf(" - room # : ");
+				nRoom = getIntegerInput();
+                
+				//check whether the address is valid or not
+				if (buildingValidityCheck(nBuilding, nRoom) != 0)
+				{
+					printf(" -----------> Invalid building/room number (%i,%i)\n", nBuilding, nRoom);
+					continue;
+				}
+				
+                //try to find packages destinated to the address
+				if (str_findStorage(nBuilding, nRoom) == 0)
+				{
+					printf(" -----------> Failed to find my package!\n");
+				}
+				
+				break;
+				
+			default:
+				printf(" -----------> Wrong cmd!! input again!..\n");
+				break;
+		}
+		
+        //backup the status of the storage
+		if (str_backupSystem(STORAGE_FILEPATH) != 0)
+		{
+			printf("[WARNING] failed to backup the delivery system!\n");
+		}
+	} while (cmd != 0);
+	
+	str_freeSystem();
+	
+	return 0;
+}
